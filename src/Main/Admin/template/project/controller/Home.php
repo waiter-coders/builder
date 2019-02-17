d0fce5614091e4eeba79c975ac92de6acca5e341
tian.zheng <tian.zheng@zol.com.cn>
2019-02-17 15:15:37 +0800

    首页模板

---
+++ src/Main/Admin/template/project/controller/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Controller;
+
+use \Tools\Request as Request;
+
+class User extends Base
+{
+    public function getConfig()
+    {
+
+    }
+
+    public function getHtml()
+    {
+        return $this->view('');
+    }
+}
